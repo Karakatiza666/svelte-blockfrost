7f31b025c15be1afcec8c1779a7fd6c7f7d09aaa
George <bulakh.96@gmail.com>
2023-05-03 19:04:30 +0300

    Add error handling to client side requests

--- src/svelte-blockfrost-client.ts
+++ src/svelte-blockfrost-client.ts
@@ -3,7 +3,7 @@ import { paginateMethod } from '@blockfrost/blockfrost-js/lib/utils/index.js'
 import type { NetworkId } from '@stricahq/typhonjs/dist/types.js'
 import { fromBase64, fromHex, isBase64, isHex, makeHex, toHex, type Hex } from 'ts-binary-newtypes'
 import { mapObj, mapObj_, nonNull } from 'ts-practical-fp'
-import { components } from '@blockfrost/openapi';
+import { components } from '@blockfrost/openapi'
 
 const typhonNetworks = {
    'mainnet': 1,
@@ -25,15 +25,33 @@ export const showNetwork = (network: NetworkId) =>
 
 const querySuffix = (query?: Record<string, string | number | boolean>) => !query || !Object.keys(query) ? '' : '?' + new URLSearchParams(mapObj_(query)(v => v.toString()))
 
+// export class HttpError extends Error {
+//    constructor(message: string) {
+//       super(message)
+//    }
+// 	public status: number;
+// 	body: App.Error;
+// }
+const errorCause = (cause: Record<string, unknown>, message?: string) => {
+   const err = new Error(message)
+   Object.entries(cause).forEach(([k, v]) => (err as any)[k] = v)
+   return err
+}
+const onHttpError = <T>(code: number, val: T) => (error: any) =>
+   error.status && error.status === code ? val : Promise.reject(error)
+
 // Creates a wrapper with interface identical to BlockfrostAPI, that sends request to server-side svelte endpoint
 // which in turn calls Blockfrost from backend
 export const makeBlockfrostApiClient = (endpoint: string, networkId: NetworkId): BlockFrostAPI => {
    const req = (path: string, query?: Record<string, string | number>, init?: RequestInit) =>
       fetch(`${endpoint}/${showNetwork(networkId)}${path}` + querySuffix(query), init)
-         .then(res => res.json())
+         .then(res => res.ok || res.redirected
+            ? res.json()
+            : Promise.reject(errorCause({ status: res.status, statusText: res.statusText }))
+         )
    return {
-      addressesUtxos: (addr, pagination) => req(`/addresses/${addr}/utxos`, pagination),
-      addressesUtxosAsset: (addr, asset, pagination) => req(`/addresses/${addr}/utxos/${asset}`, pagination),
+      addressesUtxos: (addr, pagination) => req(`/addresses/${addr}/utxos`, pagination).catch(onHttpError(404, [])),
+      addressesUtxosAsset: (addr, asset, pagination) => req(`/addresses/${addr}/utxos/${asset}`, pagination).catch(onHttpError(404, [])),
       assetsHistory: (asset, pagination) =>
          req(`/assets/${asset}/history`, pagination),
       assetsHistoryAll: (asset, allMethodOptions = {batchSize: 100, order: 'asc'}) =>
